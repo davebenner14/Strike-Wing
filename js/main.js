b9ab5cc3aec47d37f0c81b9f221aba1871d40b0d
David Benner <119465113+davebenner14@users.noreply.github.com>
2023-09-25 13:11:26 -0400

    Add basic main.js code utilzing phaser.js

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// main.js
+
+var config = {
+  type: Phaser.AUTO,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    parent: "game-container",
+    width: 800,
+    height: 600
+  },
+  scene: [
+    BootScene,
+    PreloadScene,
+    StartMenuScene,
+    Level1Scene,
+    Level2Scene,
+    Level3Scene
+  ],
+  input: {
+    activePointers: 1
+  }
+};
+
+var game = new Phaser.Game(config);
