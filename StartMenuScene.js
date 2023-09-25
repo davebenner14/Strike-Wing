0824b747d0d05e370d6e7390dcd86d4b89917e91
David Benner <119465113+davebenner14@users.noreply.github.com>
2023-09-25 14:19:27 -0400

    Update Start Menu font

--- StartMenuScene.js
+++ StartMenuScene.js
@@ -6,18 +6,28 @@ class StartMenuScene extends Phaser.Scene {
   create() {
     this.cameras.main.setBackgroundColor("#000");
 
-    let titleText = this.add.text(100, 50, "Jump Man", {
-      fontSize: "48px",
-      fill: "#fff"
-    });
+    let titleText = this.add.text(
+      this.cameras.main.centerX - 200,
+      50,
+      "Jump Man",
+      {
+        fontFamily: "VT323",
+        fontSize: "96px",
+        fill: "#ffffff" // This is where you set the color
+      }
+    );
 
-    let pressStartText = this.add.text(300, 300, "Press Start", {
+    // Adding shadow to the text
+    titleText.setShadow(2, 2, "#333333", 2, false, true);
+
+    this.pressStartText = this.add.text(300, 300, "Press Start", {
+      fontFamily: "VT323",
       fontSize: "24px",
       fill: "#fff"
     });
 
-    this.tweens.add({
-      targets: pressStartText,
+    this.startTween = this.tweens.add({
+      targets: this.pressStartText,
       alpha: 0,
       duration: 500,
       ease: "Linear",
@@ -25,47 +35,53 @@ class StartMenuScene extends Phaser.Scene {
       repeat: -1
     });
 
+    this.input.on("pointerdown", () => {
+      this.showMenu();
+    });
+
+    this.input.keyboard.on("keydown-ENTER", () => {
+      this.showMenu();
+    });
+  }
+
+  showMenu() {
+    this.startTween.stop();
+    this.pressStartText.setAlpha(0);
     let menuItems = ["Start Game", "Story"];
     let selectedItem = 0;
     let menuTexts = [];
 
-    this.input.on("pointerdown", () => {
-      pressStartText.alpha = 0;
-      pressStartText.removeInteractive();
-
-      // Display menu items
-      for (let i = 0; i < menuItems.length; i++) {
-        let yPosition = 300 + i * 50;
-        let text = this.add.text(300, yPosition, menuItems[i], {
-          fontSize: "24px",
-          fill: "#fff"
-        });
-        text.setInteractive();
-
-        text.on("pointerdown", () => {
-          this.selectOption(selectedItem);
-        });
-
-        menuTexts.push(text);
-      }
-
-      menuTexts[selectedItem].setColor("#f00");
-
-      this.input.keyboard.on("keydown-UP", () => {
-        menuTexts[selectedItem].setColor("#fff");
-        selectedItem = (selectedItem - 1 + menuItems.length) % menuItems.length;
-        menuTexts[selectedItem].setColor("#f00");
+    for (let i = 0; i < menuItems.length; i++) {
+      let yPosition = 300 + i * 50;
+      let text = this.add.text(300, yPosition, menuItems[i], {
+        fontSize: "24px",
+        fill: "#fff"
       });
+      text.setInteractive();
 
-      this.input.keyboard.on("keydown-DOWN", () => {
-        menuTexts[selectedItem].setColor("#fff");
-        selectedItem = (selectedItem + 1) % menuItems.length;
-        menuTexts[selectedItem].setColor("#f00");
-      });
-
-      this.input.keyboard.on("keydown-ENTER", () => {
+      text.on("pointerdown", () => {
         this.selectOption(selectedItem);
       });
+
+      menuTexts.push(text);
+    }
+
+    menuTexts[selectedItem].setColor("#f00");
+
+    this.input.keyboard.on("keydown-UP", () => {
+      menuTexts[selectedItem].setColor("#fff");
+      selectedItem = (selectedItem - 1 + menuItems.length) % menuItems.length;
+      menuTexts[selectedItem].setColor("#f00");
+    });
+
+    this.input.keyboard.on("keydown-DOWN", () => {
+      menuTexts[selectedItem].setColor("#fff");
+      selectedItem = (selectedItem + 1) % menuItems.length;
+      menuTexts[selectedItem].setColor("#f00");
+    });
+
+    this.input.keyboard.on("keydown-ENTER", () => {
+      this.selectOption(selectedItem);
     });
   }
 
