0c98fb26c81ae68a3a9ceec13c2e20fe5415b919
David Benner <119465113+davebenner14@users.noreply.github.com>
2023-10-05 12:08:16 -0400

    Add level4 basic functionality

--- Level4Scene.js
+++ Level4Scene.js
@@ -33,8 +33,18 @@ export default class Level4Scene extends Phaser.Scene {
     try {
       console.log("Loading assets...");
 
-      this.initBackgroundSprite("layer0", "layer0", 3);
-      this.initBackgroundSprite("layer1", "layer1", 2.9);
+      this.initBackgroundSprite("layer11", "Layer_0011_0", 0.1); // Slowest layer
+      this.initBackgroundSprite("layer10", "Layer_0010_1", 0.15);
+      this.initBackgroundSprite("layer9", "Layer_0009_2", 0.2);
+      this.initBackgroundSprite("layer8", "Layer_0008_3", 0.25);
+      this.initBackgroundSprite("layer7", "Layer_0007_Lights", 0.3);
+      this.initBackgroundSprite("layer6", "Layer_0006_4", 0.35);
+      this.initBackgroundSprite("layer5", "Layer_0005_5", 0.4);
+      this.initBackgroundSprite("layer4", "Layer_0004_Lights", 0.45);
+      this.initBackgroundSprite("layer3", "Layer_0003_6", 0.5);
+      this.initBackgroundSprite("layer2", "Layer_0002_7", 0.55);
+      this.initBackgroundSprite("layer1", "Layer_0001_8", 0.6);
+      this.initBackgroundSprite("layer0", "Layer_0000_9", 0.65); // Fastest layer
 
       this.plane = this.add.sprite(
         this.scale.width * 0.1,
